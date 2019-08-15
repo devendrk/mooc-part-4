f1f601a923d4ea912bdfcf228bdaaf8c5e02365f
devendrk <katwaldevendra@hotmail>
2019-08-15 18:47:11 +0300

    completed step1 and step2 created blog route, refactored folder structure

--- index.js
+++ index.js
@@ -4,22 +4,4 @@ const config = require('./utils/config')
 
 const server = http.createServer(app)
 
-<<<<<<< HEAD
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connection to MongoDB:', error.message)
-  })
-
-// const cors = require('cors')
-app.use(bodyParser.json())
-app.use(express.json())
-app.use(blogRouter)
-
-const port = process.env.PORT
-app.listen(port, () => console.log(`Server running on port ${port}`))
-=======
 server.listen(config.PORT, () => console.log(`server is running on port ${config.PORT}`))
->>>>>>> blogList-step-1-2
--- utils/middleware.js
+++ utils/middleware.js
@@ -10,12 +10,12 @@ const unkownEndPoint = (req, res) => {
   res.status(404).send({ error: 'unknown endpoint' })
 }
 
-const errorHandler = (error,req,res,next)=>{
+const errorHandler = (error, req, res, next) => {
   console.log(error.message)
-  if(error.name === 'CastError' && error.kind === 'ObjectId'){
-    return res.status(400).send({error:'malformatted id'})
-  }else if (error.name === 'ValidationError'){
-    return res.status(400).json({error:error.message})
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
   }
   next(error)
 }
