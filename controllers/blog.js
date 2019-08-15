069300598f10f1d3e6e35e19228f3de7e48736c3
devendrk <katwaldevendra@hotmail>
2019-08-15 18:33:34 +0300

    completed step1 and step2 created blog route, refactored folder structure

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,8 +1,7 @@
-const express = require('express')
+const router = require('express').Router()
 const Blog = require('../models/blog')
-const router = new express.Router()
 
-router.post('/api/blogs', async (req, res) => {
+router.post('/', async (req, res) => {
   const blog = new Blog(req.body)
   try {
     await blog.save()
@@ -12,12 +11,52 @@ router.post('/api/blogs', async (req, res) => {
   }
 })
 
-router.get('/api/blogs', async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const blogs = await Blog.find({})
     res.send(blogs)
   } catch (error) {
     res.status(500).send(error)
+  }
+})
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const blog = await Blog.findById(req.params.id)
+    if (blog) {
+      res.send(blog)
+    } else {
+      Response.status(404).end()
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+// update blog 
+router.put('/:id', async (req, res, next) => {
+  try {
+    const body = req.body
+
+    const blog = {
+      title: body.title,
+      author: body.author,
+      url: body.url,
+    }
+    const blogUpdate = await Blog.findOneAndUpdate(req.params.id, blog, { new: true })
+    res.send(blogUpdate)
+  } catch (error) {
+    next(error)
+  }
+})
+
+//delete blog
+router.delete('/:id', async (req, res, next) => {
+  try {
+    await Blog.findByIdAndRemove(req.params.id)
+    res.status(204).end()
+  } catch (error) {
+    next(error)
   }
 })
 
--- index.js
+++ index.js
@@ -1,24 +1,7 @@
+const app = require('./app')
+const http = require('http')
 const config = require('./utils/config')
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const blogRouter = require('./controllers/blog')
-const mongoose = require('mongoose')
 
-console.log('connecting to', config.MONGODB_URI)
+const server = http.createServer(app)
 
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connection to MongoDB:', error.message)
-  })
-
-const cors = require('cors')
-app.use(bodyParser.json())
-app.use(express.json())
-app.use(blogRouter)
-
-const port = process.env.PORT
-app.listen(port, () => console.log(`Server running on port ${port}`))
+server.listen(config.PORT, () => console.log(`server is running on port ${config.PORT}`))
